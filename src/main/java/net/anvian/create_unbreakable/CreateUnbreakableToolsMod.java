7aded0f8847a1e32344e7f3df54022e8531f427f
andre-carbajal <carbajalvargasandre@gmail.com>
2023-12-30 17:21:02 -0500

    item and item group added

--- src/main/java/net/anvian/create_unbreakable/CreateUnbreakableToolsMod.java
+++ src/main/java/net/anvian/create_unbreakable/CreateUnbreakableToolsMod.java
@@ -1,22 +1,31 @@
 package net.anvian.create_unbreakable;
 
-import com.simibubi.create.Create;
-
-import io.github.fabricators_of_create.porting_lib.util.EnvExecutor;
-import net.fabricmc.api.ModInitializer;
-
-import net.minecraft.resources.ResourceLocation;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.simibubi.create.Create;
+import com.simibubi.create.foundation.data.CreateRegistrate;
+
+import io.github.fabricators_of_create.porting_lib.util.EnvExecutor;
+import io.github.fabricators_of_create.porting_lib.util.ItemGroupUtil;
+import net.anvian.create_unbreakable.item.ModItem;
+import net.anvian.create_unbreakable.item.tab.ModCreativeTab;
+import net.fabricmc.api.ModInitializer;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.CreativeModeTab;
 
 public class CreateUnbreakableToolsMod implements ModInitializer {
 	public static final String ID = "create_unbreakable";
 	public static final String NAME = "Create Unbreakable Tools";
 	public static final Logger LOGGER = LoggerFactory.getLogger(NAME);
+	public static final CreateRegistrate REGISTRATE = CreateRegistrate.create(ID);
+	public static final CreativeModeTab TAB = new ModCreativeTab(ItemGroupUtil.expandArrayAndGetId(), ID);
 
 	@Override
 	public void onInitialize() {
+		ModItem.register();
+		REGISTRATE.register();
+
 		LOGGER.info("Create addon mod [{}] is loading alongside Create [{}]!", NAME, Create.VERSION);
 		LOGGER.info(EnvExecutor.unsafeRunForDist(
 				() -> () -> "{} is accessing Porting Lib from the client!",
---
+++ src/main/java/net/anvian/create_unbreakable/item/ModItem.java
@@ -0,0 +1,19 @@
+package net.anvian.create_unbreakable.item;
+
+import com.tterrag.registrate.util.entry.ItemEntry;
+
+import net.anvian.create_unbreakable.CreateUnbreakableToolsMod;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Rarity;
+
+public class ModItem {
+	static {
+		CreateUnbreakableToolsMod.REGISTRATE.creativeModeTab(() ->CreateUnbreakableToolsMod.TAB);
+	}
+	public static final ItemEntry<Item> UNBREAKABLE_GEM = CreateUnbreakableToolsMod.REGISTRATE.item("unbreakable_gem", Item::new)
+			.properties(p -> p.rarity(Rarity.RARE))
+			.properties(p -> p.stacksTo(1))
+			.register();
+
+	public static void register(){}
+}
