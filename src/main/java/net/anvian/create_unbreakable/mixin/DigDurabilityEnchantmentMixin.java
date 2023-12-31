ebd56ff89f8ae22688163044d85f7d38397c3380
andre-carbajal <carbajalvargasandre@gmail.com>
2023-12-31 13:12:02 -0500

    fix

--- src/main/java/net/anvian/create_unbreakable/mixin/DigDurabilityEnchantmentMixin.java
+++ src/main/java/net/anvian/create_unbreakable/mixin/DigDurabilityEnchantmentMixin.java
@@ -1,20 +1,12 @@
 package net.anvian.create_unbreakable.mixin;
 
-import net.minecraft.util.RandomSource;
+import org.spongepowered.asm.mixin.Mixin;
+
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.enchantment.DigDurabilityEnchantment;
-
 import net.minecraft.world.item.enchantment.Enchantment;
-
 import net.minecraft.world.item.enchantment.EnchantmentCategory;
-
-import net.minecraft.world.item.enchantment.MendingEnchantment;
-
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(DigDurabilityEnchantment.class)
 public class DigDurabilityEnchantmentMixin extends Enchantment {
@@ -23,18 +15,7 @@ public class DigDurabilityEnchantmentMixin extends Enchantment {
 	}
 
 	@Override
-	protected boolean checkCompatibility(Enchantment other) {
-		return !(other instanceof MendingEnchantment) && super.checkCompatibility(other);
-	}
-
-	@Inject(method = "canEnchant", at = @At(value = "HEAD"), cancellable = true)
-	private void dontAcceptUnbreakableItems(ItemStack stack, CallbackInfoReturnable<Boolean> cir) {
-		cir.setReturnValue(stack.getTag() != null && !stack.getTag().getBoolean("Unbreakable"));
-	}
-
-	@Inject(method = "shouldIgnoreDurabilityDrop", at = @At(value = "HEAD"), cancellable = true)
-	private static void makeUnbreakable(ItemStack stack, int level, RandomSource random, CallbackInfoReturnable<Boolean> cir) {
-		stack.setDamageValue(0);
-		cir.setReturnValue(true);
+	public boolean canEnchant(ItemStack stack) {
+		return stack.getTag() !=null && !stack.getTag().getBoolean("Unbreakable");
 	}
 }
