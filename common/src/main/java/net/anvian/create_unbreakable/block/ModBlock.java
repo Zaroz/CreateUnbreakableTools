a97bc9ad5206faf1b472198d5831063eb3cd8696
andre-carbajal <carbajalvargasandre@gmail.com>
2024-01-10 19:38:01 -0500

    mak block explode

--- common/src/main/java/net/anvian/create_unbreakable/block/ModBlock.java
+++ common/src/main/java/net/anvian/create_unbreakable/block/ModBlock.java
@@ -3,6 +3,8 @@ package net.anvian.create_unbreakable.block;
 import com.simibubi.create.foundation.data.CreateRegistrate;
 import com.tterrag.registrate.util.entry.BlockEntry;
 import net.anvian.create_unbreakable.CreateUnbreakableToolsMod;
+import net.anvian.create_unbreakable.block.custom.EternalBlock;
+import net.anvian.create_unbreakable.item.ModItem;
 import net.anvian.create_unbreakable.item.tab.ModGroup;
 import net.minecraft.world.level.block.Block;
 
@@ -13,12 +15,14 @@ public class ModBlock {
         REGISTRATE.creativeModeTab(() -> ModGroup.GROUP);
     }
 
-    public static final BlockEntry<Block> ETERNAL_PIECE_BLOCK = REGISTRATE.block("eternal_piece_block", Block::new)
+    public static final BlockEntry<EternalBlock> ETERNAL_PIECE_BLOCK = REGISTRATE.block("eternal_piece_block",
+                    (p) -> new EternalBlock(p, ModItem.ETERNAL_PIECE.get()))
             .properties(p -> p.strength(5.0F, 6.0F))
             .simpleItem()
             .register();
 
-    public static final BlockEntry<Block> ETERNAL_ORB_BLOCK = REGISTRATE.block("eternal_orb_block", Block::new)
+    public static final BlockEntry<EternalBlock> ETERNAL_ORB_BLOCK = REGISTRATE.block("eternal_orb_block",
+                    (p) -> new EternalBlock(p, ModItem.ETERNAL_ORB.get()))
             .properties(p -> p.strength(5.0F, 6.0F))
             .simpleItem()
             .register();
