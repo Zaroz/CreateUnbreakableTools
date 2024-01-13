878807f29d43af0557b8c6be59b96230b9b39301
andre-carbajal <carbajalvargasandre@gmail.com>
2024-01-13 16:52:35 -0500

    ResourceLocation ID removed

--- src/main/java/net/anvian/create_unbreakable/mixin/MendingEnchantmentMixin.java
+++ src/main/java/net/anvian/create_unbreakable/mixin/MendingEnchantmentMixin.java
@@ -1,12 +1,12 @@
 package net.anvian.create_unbreakable.mixin;
 
+import org.spongepowered.asm.mixin.Mixin;
+
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.EnchantmentCategory;
 import net.minecraft.world.item.enchantment.MendingEnchantment;
-
-import org.spongepowered.asm.mixin.Mixin;
 
 @Mixin(MendingEnchantment.class)
 public class MendingEnchantmentMixin extends Enchantment {
